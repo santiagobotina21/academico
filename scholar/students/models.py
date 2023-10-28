7fad10c7cd88a0eecb0318a2f36a0fb60ecdda0a
Santiago <santiagobotina21@gmail.com>
2023-10-27 19:58:36 -0500

    Nuevo campo

--- scholar/students/models.py
+++ scholar/students/models.py
@@ -3,4 +3,5 @@ from django.db import models
 class User(models.Model):
     emai=models.CharField(max_length=100)
     password=models.CharField(max_length=500)
+    dni=models.CharField(max_length=10,default='')
 # Create your models here.
